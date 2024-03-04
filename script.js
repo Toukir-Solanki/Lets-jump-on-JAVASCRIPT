320881350f857050b937cd676a906b640cf17f8a
Toukir-Solanki <toukirsolanki@gmail.com>
2024-03-04 09:43:47 +0530

    stated

---
+++ script.js
@@ -0,0 +1,17 @@
+const name = "toukir"
+const age = 23
+
+
+
+const toukir = `I'm ${name} a ${age} year old student`
+console.log(toukir)
+
+
+
+const agebaby = 14;
+
+if(agebaby >= 18){
+     console.log(`toukir can start driving lincese`);
+}else{
+    console.log(`toukir have to wait for ${18 - agebaby} years`);
+}
