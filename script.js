21ad384b8f4a2ffdf50cb119f5426451d260fa2a
Toukir-Solanki <toukirsolanki@gmail.com>
2024-03-04 12:49:34 +0530

    learned some fundamentals about javasciprt

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const name = "toukir"
 const age = 23
 
@@ -15,3 +17,31 @@ if(agebaby >= 18){
 }else{
     console.log(`toukir have to wait for ${18 - agebaby} years`);
 }
+
+
+
+
+// ---------------------------------------->//
+
+function toukiSolanki(){
+    console.log('my name is toukiSolanki');
+}
+
+toukiSolanki();
+
+function fruitProcess(apples, organes){
+    console.log(apples, organes);
+    const juice = `Juice with ${apples} apples & ${organes} oragnes`
+    return juice;
+}
+
+
+const save = fruitProcess(5,0)
+console.log(save);
+
+
+function calcAge1(bithyear){
+    return 2024-bithyear;
+}
+
+console.log(calcAge1(1999));
